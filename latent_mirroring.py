e8061c23d87b1cfe4ae51d2d0c982997d4650fe8
DepFA <35278260+dfaker@users.noreply.github.com>
2022-11-03 22:14:32 +0000

    add guard to make sure call-back only runs when loaded

--- latent_mirroring.py
+++ latent_mirroring.py
@@ -1,4 +1,3 @@
-
 import torch
 import modules.scripts as scripts
 import gradio as gr
@@ -17,31 +16,30 @@ class Script(scripts.Script):
         mirror_mode = gr.Radio(label='Mirror application mode', choices=['None', 'Alternate Steps', 'Blend Average'], value='Alternate Steps', type="index")
         mirror_style = gr.Radio(label='Mirror style', choices=['Vertical Mirroring', 'Horizontal Mirroring', '90 Degree Rotation', '180 Degree Rotation'], value='Vertical Mirroring', type="index")
         mirroring_max_step_fraction = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, label='Maximum steps fraction to mirror at', value=0.25)
+        self.run_callback = False
         return [mirror_mode, mirror_style, mirroring_max_step_fraction]
 
     def denoise_callback(self, params):
 
-            print(params.sampling_step)
-            if params.sampling_step < params.total_sampling_steps*self.mirroring_max_step_fraction:
-                print(params.sampling_step, 'ACTION')
-                if self.mirror_mode == 1:
-                    if self.mirror_style == 0:
-                        params.x[:, :, :, :] = torch.flip(params.x, [3])
-                    elif self.mirror_style == 1:
-                        params.x[:, :, :, :] = torch.flip(params.x, [2])
-                    elif self.mirror_style == 2:
-                        params.x[:, :, :, :] = torch.rot90(params.x, dims=[2, 3])
-                    elif self.mirror_style == 3:
-                        params.x[:, :, :, :] = torch.rot90(torch.rot90(params.x, dims=[2, 3]), dims=[2, 3])
-                elif self.mirror_mode == 2:
-                    if self.mirror_style == 0:
-                        params.x[:, :, :, :] = (torch.flip(params.x, [3]) + params.x)/2
-                    elif self.mirror_style == 1:
-                        params.x[:, :, :, :] = (torch.flip(params.x, [2]) + params.x)/2
-                    elif self.mirror_style == 2:
-                        params.x[:, :, :, :] = (torch.rot90(params.x, dims=[2, 3]) + params.x)/2
-                    elif self.mirror_style == 3:
-                        params.x[:, :, :, :] = (torch.rot90(torch.rot90(params.x_in, dims=[2, 3]), dims=[2, 3]) + params.x_in)/2
+        if self.run_callback and params.sampling_step < params.total_sampling_steps*self.mirroring_max_step_fraction:
+            if self.mirror_mode == 1:
+                if self.mirror_style == 0:
+                    params.x[:, :, :, :] = torch.flip(params.x, [3])
+                elif self.mirror_style == 1:
+                    params.x[:, :, :, :] = torch.flip(params.x, [2])
+                elif self.mirror_style == 2:
+                    params.x[:, :, :, :] = torch.rot90(params.x, dims=[2, 3])
+                elif self.mirror_style == 3:
+                    params.x[:, :, :, :] = torch.rot90(torch.rot90(params.x, dims=[2, 3]), dims=[2, 3])
+            elif self.mirror_mode == 2:
+                if self.mirror_style == 0:
+                    params.x[:, :, :, :] = (torch.flip(params.x, [3]) + params.x)/2
+                elif self.mirror_style == 1:
+                    params.x[:, :, :, :] = (torch.flip(params.x, [2]) + params.x)/2
+                elif self.mirror_style == 2:
+                    params.x[:, :, :, :] = (torch.rot90(params.x, dims=[2, 3]) + params.x)/2
+                elif self.mirror_style == 3:
+                    params.x[:, :, :, :] = (torch.rot90(torch.rot90(params.x_in, dims=[2, 3]), dims=[2, 3]) + params.x_in)/2
 
     def run(self, p, mirror_mode, mirror_style, mirroring_max_step_fraction):
 
@@ -53,4 +51,8 @@ class Script(scripts.Script):
             on_cfg_denoiser(self.denoise_callback)
             self.callbacks_added = True
 
-        return processing.process_images(p)
+        self.run_callback = True
+        result = processing.process_images(p)
+        self.run_callback = False
+
+        return result
