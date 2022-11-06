0f7400e9ccbace31fce0b1c5a52b2c82f5c495f2
DepFA <35278260+dfaker@users.noreply.github.com>
2022-11-06 00:56:31 +0000

    Default mirroring to none

--- scripts/latent_mirroring.py
+++ scripts/latent_mirroring.py
@@ -15,7 +15,7 @@ class Script(scripts.Script):
         return scripts.AlwaysVisible
 
     def ui(self, is_img2img):
-        mirror_mode = gr.Radio(label='Mirror application mode', choices=['None', 'Alternate Steps', 'Blend Average'], value='Alternate Steps', type="index")
+        mirror_mode = gr.Radio(label='Mirror application mode', choices=['None', 'Alternate Steps', 'Blend Average'], value='None', type="index")
         mirror_style = gr.Radio(label='Mirror style', choices=['Vertical Mirroring', 'Horizontal Mirroring', '90 Degree Rotation', '180 Degree Rotation'], value='Vertical Mirroring', type="index")
         mirroring_max_step_fraction = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, label='Maximum steps fraction to mirror at', value=0.25)
         self.run_callback = False
